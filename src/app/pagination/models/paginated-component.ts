f76f04fecfd4bf1e600c8f36a70a81233a497f8e
danr <danr@siemplify.co>
2020-02-05 08:58:39 +0200

    clean paginated component

--- src/app/pagination/models/paginated-component.ts
+++ src/app/pagination/models/paginated-component.ts
@@ -8,10 +8,10 @@ export abstract class PaginatedComponent<T = any> implements OnInit, OnDestroy {
 	@Input() config: Partial<PaginatorConfig>;
 
 	isLoading$: Observable<boolean>;
-	dataSource$: Observable<T[]>;
-	pagination$: Observable<PaginationResponse<T>>;
 
-	constructor(protected paginator?: Paginator<T>) {
+	abstract pagination$: Observable<PaginationResponse<T>>;
+
+	constructor(protected paginator: Paginator<T>) {
 		this.initializeSelectors();
 	}
 
@@ -55,14 +55,9 @@ export abstract class PaginatedComponent<T = any> implements OnInit, OnDestroy {
 		this.paginator.lastPage();
 	}
 
-	getPaginator() {
-		return this.paginator;
-	}
-
 	protected initializeSelectors() {
 		if (this.paginator) {
 			this.isLoading$ = this.paginator.isLoading$;
-			this.dataSource$ = this.paginator.dataSource.selectData(false) as Observable<T[]>;
 		}
 	}
 }
